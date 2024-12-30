f9a0ff2b26ae1e52640cf5567e2be01e6e54bc9b
Caleb <67715898+CalebMartim@users.noreply.github.com>
2024-12-30 03:34:50 -0300

    Update lca.hpp

--- grafos/lca.hpp
+++ grafos/lca.hpp
@@ -1,8 +1,8 @@
 /* 
-		[Notas]:
-		Estamos considerando a árvore enraizada em 1. 
-    g[u] é a lista de adjacência do vértice u.
-		Rode pre() antes de usar as funções dist e lca.
+  [Notas]:
+  Estamos considerando a árvore enraizada em 1. 
+  g[u] é a lista de adjacência do vértice u.
+  Rode pre() antes de usar as funções dist e lca.
 */ 
 
 #include <vector>
@@ -16,37 +16,37 @@ int anc[N][K + 1];
 int d[N];
 
 void dfs(int u, int p) {
-	d[u] = d[p] + 1;
+  d[u] = d[p] + 1;
   anc[u][0] = p;
   for (int & v : g[u])
     if (v != p)	
-    	dfs(v, u);  
+      dfs(v, u);  
 }
 
 void pre(){
-	dfs(1, 1);
-	for (int i = 1; i <= K; ++i)
-		for (int u = 1; u < N; ++u)
-			anc[u][i] = anc[anc[u][i - 1]][i - 1];			
+  dfs(1, 1);
+  for (int i = 1; i <= K; ++i)
+    for (int u = 1; u < N; ++u)
+      anc[u][i] = anc[anc[u][i - 1]][i - 1];			
 }
 
 int lca(int a, int b) {
-	if (d[b] > d[a])
-		swap(a, b);
-	int dif = d[a] - d[b];
-	for (int i = K; i >= 0; --i)
-		if (dif & (1 << i)) 
-			a = anc[a][i];
-	if (a == b)
-		return a;
-	for (int i = K; i >= 0; --i) 
-		if (anc[a][i] != anc[b][i]) {
-			a = anc[a][i];
-			b = anc[b][i];
-		}
-	return anc[a][0];
+  if (d[b] > d[a])
+    swap(a, b);
+  int dif = d[a] - d[b];
+  for (int i = K; i >= 0; --i)
+    if (dif & (1 << i)) 
+      a = anc[a][i];
+  if (a == b)
+    return a;
+  for (int i = K; i >= 0; --i) 
+    if (anc[a][i] != anc[b][i]) {
+      a = anc[a][i];
+      b = anc[b][i];
+    }
+  return anc[a][0];
 }
 
 int dist(int a, int b) {
-	return d[a] + d[b] - 2 * d[lca(a, b)];
+  return d[a] + d[b] - 2 * d[lca(a, b)];
 }
