8eb5b02fe64d3f8b6d53533ac57ac012bd7146e7
Caleb <67715898+CalebMartim@users.noreply.github.com>
2025-01-01 12:40:12 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -19,11 +19,11 @@ void out(string x){cout<<x<<'\n';}
 Tc ...T>void in(T&...x){((cin>>x),...);}
 Tc ...T>void out_esp(T...x){((cout<<x<<' '),...);}
 Tc ...T>void out(T...x){((out_esp(x)),...);cout<<'\n';}
-Tc T, Tc _>class C>void in(C<T>&v){for(T&x:v)in(x);}
+Tc T,Tc _>class C>void in(C<T>&v){for(T&x:v)in(x);}
 Tc T,Tc _>class C>void out(C<T>v){for(T x:v)out_esp(x);cout<<'\n';}
 Tc T,Tc _>class S,Tc _>class C> void in(C<S<T>>&v){for(S<T>&x:v)in(x);}
 Tc T,Tc _>class S,Tc _>class C> void out(C<S<T>>v){for(S<T>x:v)out(x);}
-template<Tc _> class C> void out(C<string> x){for(auto y:x) out_esp(y);out();};
+template<Tc _>class C>void out(C<string> x){for(auto y:x) out_esp(y);out();};
 
 int main(){
   cin.tie(nullptr)->sync_with_stdio(false);
