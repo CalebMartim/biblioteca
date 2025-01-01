fc85f97e3de688c98b2ba2ebbcea0f9cc8ef48fd
Caleb <67715898+CalebMartim@users.noreply.github.com>
2025-01-01 12:38:44 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -13,12 +13,17 @@ using namespace std;
 #define dbg(...)
 #endif
 Tc T>using V=vector<T>;
-Tc T, class S>using P=pair<T,S>;
-Tc ...T>void in(T&...x){((cin >> x),...);}
-Tc T, Tc _>class C>void in(C<T>&v){for(T&x:v)cin>>x;}
+Tc T,class S>using P=pair<T,S>;
+void in(string&x){cin>>x;}
+void out(string x){cout<<x<<'\n';}
+Tc ...T>void in(T&...x){((cin>>x),...);}
 Tc ...T>void out_esp(T...x){((cout<<x<<' '),...);}
 Tc ...T>void out(T...x){((out_esp(x)),...);cout<<'\n';}
-Tc T, Tc _>class C>void out(C<T>&v){for(const T&x:v)out_esp(x);cout<<'\n';}
+Tc T, Tc _>class C>void in(C<T>&v){for(T&x:v)in(x);}
+Tc T,Tc _>class C>void out(C<T>v){for(T x:v)out_esp(x);cout<<'\n';}
+Tc T,Tc _>class S,Tc _>class C> void in(C<S<T>>&v){for(S<T>&x:v)in(x);}
+Tc T,Tc _>class S,Tc _>class C> void out(C<S<T>>v){for(S<T>x:v)out(x);}
+template<Tc _> class C> void out(C<string> x){for(auto y:x) out_esp(y);out();};
 
 int main(){
   cin.tie(nullptr)->sync_with_stdio(false);
