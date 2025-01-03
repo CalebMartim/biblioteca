dde066460281ffe352a3f7774e8caef1979c03c3
Caleb <67715898+CalebMartim@users.noreply.github.com>
2025-01-03 00:17:17 -0300

    Policy based data structures

---
+++ miscelânia/policy based data structures.hpp
@@ -0,0 +1,25 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <chrono>
+using namespace std;
+using namespace __gnu_pbds;
+
+// Ordered Set //
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+// GP Hash Table //
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t seed = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + seed);
+    }
+};
+typedef gp_hash_table<int, int, custom_hash> hash_table;
+
