22e1fae11f9011d2c8a79c3bc8901016b2065953
Caleb Martim <67715898+CalebMartim@users.noreply.github.com>
2025-01-17 08:27:38 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <array>
 using namespace std;
 #define ll long long
 #define pb push_back
-#define Tc template <class
-#define sz(v) (int) v.size()
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
+#define ii array<int,2>
+#define pll array<ll,2>
+#define Tc template<class
+#define sz(v) (int)size(v)
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define iter(i,e) for(int i=0;i<e;++i) 
+#define reti(i,e) for(int i=e;i>=0;--i)
 #ifdef DEBUG 
 #include "debug.hpp"
 #else
 #define dbg(...)
+#define dbg_esp(...)
 #endif
 Tc T>using V=vector<T>;
-Tc T,class S>using P=pair<T,S>;
 void in(string&x){cin>>x;}
 void out(string x){cout<<x<<'\n';}
 Tc ...T>void in(T&...x){((cin>>x),...);}
@@ -21,8 +26,8 @@ Tc ...T>void out_esp(T...x){((cout<<x<<' '),...);}
 Tc ...T>void out(T...x){((out_esp(x)),...);cout<<'\n';}
 Tc T,Tc _>class C>void in(C<T>&v){for(T&x:v)in(x);}
 Tc T,Tc _>class C>void out(C<T>v){for(T x:v)out_esp(x);cout<<'\n';}
-Tc T,Tc _>class S,Tc _>class C> void in(C<S<T>>&v){for(S<T>&x:v)in(x);}
-Tc T,Tc _>class S,Tc _>class C> void out(C<S<T>>v){for(S<T>x:v)out(x);}
+Tc T,Tc _>class S,Tc _>class C>void in(C<S<T>>&v){for(S<T>&x:v)in(x);}
+Tc T,Tc _>class S,Tc _>class C>void out(C<S<T>>v){for(S<T>x:v)out(x);}
 template<Tc _>class C>void out(C<string> x){for(auto y:x) out_esp(y);out();};
 
 int main(){
