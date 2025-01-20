838dfd88ca4ceddee75dc022ed85f18fc73be124
Caleb Martim <calebmartim2113@gmail.com>
2025-01-19 23:10:41 -0300

    Adicionado icpc template

---
+++ icpc_template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pb push_back
+#define ii array<int,2>
+#define sz(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define iter(i,b,e) for(int i=(b);i<(e);++i)
+#define reti(i,e,b) for(int i=(e);i>=(b);--i)
+
+int main(){
+  cin.tie(nullptr)->sync_with_stdio(false);
+  
+}
