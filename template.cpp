6c054c6afbb69714bf07a095c77c4434cf95121b
Caleb Martim <67715898+CalebMartim@users.noreply.github.com>
2025-01-26 11:22:58 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,16 +6,10 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;
-#define ll long long
 #define pb push_back
-#define ld long double
-#define ii array<int,2>
-#define pll array<ll,2>
 #define Tc template<class
 #define sz(v) (int)size(v)
-#define rbl(x,v) for(auto x:v)
-#define rblr(x,v) for(auto&x:v)
+#define rbl(x,v) for(auto&x:v)
 #define all(v) v.begin(),v.end()
 #define rall(v) v.rbegin(),v.rend()
 #define iter(i,b,e) for(int i=(b);i<(e);++i) 
@@ -26,16 +20,21 @@ using namespace std;
 #define dbg(...)
 #define dbg_esp(...)
 #endif
+using namespace std;
+using ll = long long;
+using ld = long double;
 Tc T>using V=vector<T>;
+using ii = array<int,2>;
+using pll = array<ll,2>;
 void in(string&x){cin>>x;}
 void out(string x){cout<<x<<'\n';}
 Tc ...T>void in(T&...x){((cin>>x),...);}
 Tc ...T>void oute(T...x){((cout<<x<<' '),...);}
 Tc ...T>void out(T...x){((oute(x)),...);cout<<'\n';}
-Tc T,Tc _>class C>void in(C<T>&v){for(T&x:v)in(x);}
 Tc T,Tc _>class ...C>void in(C<T>&...v){(in(v),...);}
-Tc T,Tc _>class C>void out(C<T>v){for(T x:v)oute(x);cout<<'\n';}
+Tc T,Tc _>class C>void in(C<T>&v){for(T &x:v)in(x);}
 Tc T,Tc _>class ...C>void out(C<T>...v){(out(v),...);}
+Tc T,Tc _>class C>void out(C<T>v){for(T x:v)oute(x);cout<<'\n';}
 Tc T,Tc _>class S,Tc _>class C>void in(C<S<T>>&v){for(S<T>&x:v)in(x);}
 Tc T,Tc _>class S,Tc _>class C>void out(C<S<T>>v){for(S<T>x:v)out(x);}
 
