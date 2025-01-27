4b4686404634ec97d4799b63cb8629ac53d1f9e8
Caleb Martim <67715898+CalebMartim@users.noreply.github.com>
2025-01-27 14:05:07 -0400

    Update icpc_template.cpp

--- icpc_template.cpp
+++ icpc_template.cpp
@@ -3,7 +3,9 @@ using namespace std;
 #define ll long long 
 #define pb push_back
 #define ii array<int,2>
+#define pll array<ll,2>
 #define sz(v) (int)v.size()
+#define rbl(x,b) for(auto&x:v)
 #define all(v) v.begin(),v.end()
 #define rall(v) v.rbegin(),v.rend()
 #define iter(i,b,e) for(int i=(b);i<(e);++i)
