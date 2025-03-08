d05e55f85eeee2ec137dcf4f5436f254781db6df
Caleb Martim <67715898+CalebMartim@users.noreply.github.com>
2025-03-08 08:57:46 -0300

    Update template.cpp
    Consertado um bug com a entrada e saída

--- template.cpp
+++ template.cpp
@@ -1,41 +1,46 @@
-#include <map>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include <set>
+#include <map>
 #include <stack>
 #include <queue>
 #include <array>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+
 #define pb push_back
-#define Tc template<class
 #define sz(v) (int)size(v)
-#define rbl(x,v) for(auto&x:v)
 #define all(v) v.begin(),v.end()
 #define rall(v) v.rbegin(),v.rend()
+#define rbl(x,v) for(auto&x:v)
 #define iter(i,b,e) for(int i=(b);i<(e);++i) 
 #define reti(i,e,b) for(int i=(e);i>=(b);--i)
+#define Tc template<class
+
 #ifdef DEBUG 
 #include "debug.hpp"
 #else
 #define dbg(...)
 #define dbg_esp(...)
 #endif
+
 using namespace std;
 using ll = long long;
 using ld = long double;
 Tc T>using V=vector<T>;
 using ii = array<int,2>;
 using pll = array<ll,2>;
-void in(string&x){cin>>x;}
+
+void in(string&x){cin>>x;} 
+Tc ...T>void in(T&...x){((cin>>x),...);} 
+Tc T,Tc _>class C>void in(C<T>&v){for(T&x:v)in(x);} 
+Tc T,Tc _>class ...C>void in(C<T>&...v){(in(v),...);} 
+Tc T,Tc _>class S,Tc _>class C>void in(C<S<T>>&v){for(S<T>&x:v)in(x);} 
+
 void out(string x){cout<<x<<'\n';}
-Tc ...T>void in(T&...x){((cin>>x),...);}
 Tc ...T>void oute(T...x){((cout<<x<<' '),...);}
 Tc ...T>void out(T...x){((oute(x)),...);cout<<'\n';}
-Tc T,Tc _>class ...C>void in(C<T>&...v){(in(v),...);}
-Tc T,Tc _>class C>void in(C<T>&v){for(T &x:v)in(x);}
-Tc T,Tc _>class ...C>void out(C<T>...v){(out(v),...);}
 Tc T,Tc _>class C>void out(C<T>v){for(T x:v)oute(x);cout<<'\n';}
-Tc T,Tc _>class S,Tc _>class C>void in(C<S<T>>&v){for(S<T>&x:v)in(x);}
+Tc T,Tc _>class ...C>void out(C<T>...v){(out(v),...);}
 Tc T,Tc _>class S,Tc _>class C>void out(C<S<T>>v){for(S<T>x:v)out(x);}
 
 int main(){
