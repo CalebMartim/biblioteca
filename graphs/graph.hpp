f508c9e5c77246c9bc1af76f8a287c4d297461c9
calebmartim <calebmartim2113@gmail.com>
2026-02-04 15:14:13 -0300

    changed graphs section

---
+++ graphs/graph.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+#define pb push_back
+using namespace std;
+template <class T> using V = vector<T>;
+
+struct Graph{
+  int n;
+  V<V<int>> adj;
+
+  Graph(int N){
+    n = N;
+    adj.resize(n);
+  }
+
+  void add_edge(int u, int v, bool undirected = true){
+    adj[u].pb(v);
+    if (undirected) 
+      adj[v].pb(u);
+  }
+};
