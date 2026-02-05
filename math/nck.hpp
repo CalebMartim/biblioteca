f20cd012bd49302a4bc1680b1c319b2af1fd87a9
calebmartim <calebmartim2113@gmail.com>
2026-02-05 09:57:28 -0300

    faster nck

--- math/nck.hpp
+++ math/nck.hpp
@@ -1,27 +1,24 @@
 #include <vector>
-#include <cassert>
 
 #include "fast_exponentiation.hpp"
 
 using namespace std;
 
 struct Nck{
-  int maxn;
   vector<ll> f, fi;
   
-  Nck(int _maxn = (int) 1e6) {
-    maxn = _maxn;
-    f.resize(maxn + 1);
-    fi.resize(maxn + 1);
+  Nck(int n = (int) 1e6) {
+    f.resize(n + 1);
+    fi.resize(n + 1);
     f[0] = 1;
-    for (int i = 1; i <= maxn; ++i) {
+    for (int i = 1; i <= n; ++i) 
       f[i] = f[i - 1] * i % MOD;
-      fi[i] = fexp(f[i], MOD - 2);
-    }
+    fi[n] = fexp(f[n], MOD - 2);
+    for (int i = n - 1; i >= 0; --i)
+      fi[i] = fi[i + 1] * (i + 1) % MOD;
   }
 
   ll operator()(ll n, ll k) {
-    assert(k <= n);
     return f[n] * fi[n - k] % MOD * fi[k] % MOD;
   }
 };
